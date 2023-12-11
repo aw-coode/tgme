e68c46e27bfed97afea45fe18147e3fc044c35ce
ALAWE–CODE <142848462+aw-coode@users.noreply.github.com>
2023-12-11 06:15:51 +0300

    update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 21627756))
+    APP_ID = int(os.environ.get("APP_ID", 29515967))
 
-    API_HASH = os.environ.get("API_HASH", "fe77fbf0cae9f7f5ece37659e2466cf1")
+    API_HASH = os.environ.get("API_HASH", "2d82195bf121962008d599998947ae71")
 
     
